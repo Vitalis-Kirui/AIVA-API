a4f22893b03c2b3cdbd5b057b933d2a23378423e
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-08 16:58:28 +0300

    creating expenses model

---
+++ models/expenses.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+// Expenses model
+const expensesmodel = new schema({
+    name: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    totalcost: {
+        type: Number,
+        require: true
+    }
+});
+
+module.exports = mongoose.model('Expense', expensesmodel, 'expense');
