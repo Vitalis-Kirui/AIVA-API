9c6a93adf5e30f08de18b12df64e6c04561943ea
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-08 17:12:08 +0300

    creating cyber model

---
+++ models/cyber.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+// Cyber services model
+const cybermodel = new schema({
+    clientsname: {
+        type: String,
+        require: true
+    },
+    servicename: {
+        type: String,
+        require: true
+    },
+    quantity: {
+        type: Number
+    },
+    totalcost: {
+        type: Number,
+        require: true
+    },
+    payment: {
+        type: String,
+        require: true
+    },
+    transactioncode: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Cyber', cybermodel,'cyber');
