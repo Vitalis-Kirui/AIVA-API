71277e8565e3be32d1f9ba69d17cf285b763d47e
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-11 17:28:08 +0300

    debugging expenses controller functions and routes

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,60 +1,60 @@
 const Expenses = require('../models/expenses');
 
 // Saving new expense
-const newexpense = (req, res) => { 
+const newexpense = async (req, res) => {
+  try {
+    const expenses = req.body;
 
-    let expensedata = req.body;
+    const expense = new Expenses(expenses);
 
-    Expenses.save(expensedata)
-        .then((success) => {
-            res.json({
-                message: 'Expense registered successfully'
-            })
-        })
-        .catch((error) => { 
-            console.log(error)
-            res.json({
-                message: 'There was an error registering the expense'
-            })
-        });
+    await expense.save();
 
+    res.status(201).json({
+      success: true,
+      message: "Expense saved successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error saving the expense",
+      error: error.message,
+    });
+  }
 };
 
 // Fetching all registered expenses
 const allexpenses = (req, res) => { 
 
     Expenses.find()
-        .sort({ 'createdAt': 1 })
-        .then((registeredexpense) => { 
+      .sort({ createdAt: 1 })
+      .then((registeredexpense) => {
+        let totalexpenses = registeredexpense.length;
 
-            let totalexpenses = registeredexpense.length;
+        res.json({
+          total: totalexpenses,
+          expenses: registeredexpense,
+        });
+      })
 
-            res.json({
-                
-                total: totalexpenses,
-                expenses: registeredexpense
-            })
+      .catch((error) => {
+        console.log(error);
+        res.json({
+          message: "An error occurred while fetching expenses",
+          error: error,
+        });
+      });
 
-                .catch((error) => { 
-                    console.log(error);
-                    res.json({
-                        message: 'An error occurred while fetching expenses',
-                        error: error
-                    });
-                })
-
-        })
 };
 
 // Today's expenses
 const todayexpenses = (req, res) => { 
 
     Expenses.find({ createdAt: { $lt: new Date(), $gt: new Date(new Date().getTime() - (24 * 60 * 60 * 1000)) } }).sort({ createdAt: -1 })
-        .then((todayexpenses) => {
+        .then((todayexpsense) => {
 
-            let totalexpenses = todayexpenses.length;
+            let totalexpenses = todayexpsense.length;
 
-            res.json({ total: totalexpenses, expenses: todayexpenses })
+            res.json({ total: totalexpenses, expenses: todayexpsense })
         })
         .catch((error) => {
             console.log(error)
