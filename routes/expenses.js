d5dbcd11a52e972482f38010b20b315abde9b269
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-11 14:45:48 +0300

    expenses routes

---
+++ routes/expenses.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const expensescontrollers = require('../controllers/expenses');
+
+// New expense route
+router.post('/new-expense', expensescontrollers.newexpense);
+
+// All expenses route
+router.get('/all-expenses', expensescontrollers.allexpenses);
+
+// Today expenses route
+router.get('/today-expenses', expensescontrollers.todayexpenses);
