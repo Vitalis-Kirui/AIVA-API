80b1a49c7e0975c03c17d6cc1e6d3839809ab29f
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-11 17:15:11 +0300

    debugging cyber controller functions and routes

--- controllers/cyber.js
+++ controllers/cyber.js
@@ -1,49 +1,50 @@
 const Cyber = require('../models/cyber');
 
 // Saving new cyber service
-const newcyberservice = (req, res) => { 
+const newcyberservice = async (req, res) => {
+  try {
+    const servicedata = req.body;
 
-    let servicedata = req.body;
+    const cyberservice = new Cyber({
+      servicedata,
+    });
 
-    Cyber.save(servicedata)
-        .then((success) => {
-            res.json({
-                message: 'Cyber service registered successfully'
-            })
-        })
-        .catch((error) => { 
-            console.log(error)
-            res.json({
-                message: 'There was an error registering the cyber service'
-            })
-        });
+    await cyberservice.save();
 
+    res.status(201).json({
+      success: true,
+      message: "Service saved successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error saving the service",
+      error: error.message,
+    });
+  }
 };
 
 // Fetching all registered cyber services
 const allcyberservices = (req, res) => { 
 
     Cyber.find()
-        .sort({ 'createdAt': 1 })
-        .then((registeredservices) => { 
+      .sort({ createdAt: 1 })
+      .then((registeredservices) => {
+        let totalservices = registeredservices.length;
 
-            let totalservices = registeredservices.length;
+        res.json({
+          total: totalservices,
+          cyberservices: registeredservices,
+        });
+      })
 
-            res.json({
-                
-                total: totalservices,
-                cyberservices: registeredservices
-            })
-
-                .catch((error) => { 
-                    console.log(error);
-                    res.json({
-                        message: 'An error occurred while fetching cyber services',
-                        error: error
-                    });
-                })
-
-        })
+      .catch((error) => {
+        console.log(error);
+        res.json({
+          message: "An error occurred while fetching cyber services",
+          error: error,
+        });
+      });
 };
 
 // Today's cyber services
