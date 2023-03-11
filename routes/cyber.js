3855c67c4ec4e2c7fd60c0792de2727f90809b9a
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-11 14:42:26 +0300

    cyber services routes

---
+++ routes/cyber.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const cybercontrollers = require('../controllers/cyber');
+
+// New cyber service route
+router.post('/new-cyber-service', cybercontrollers.newcyberservice);
+
+// All cyber services route
+router.get('/all-cyber-services', cybercontrollers.allcyberservices);
+
+// Today's cyber service route
+router.get('/today-cyber-services', cybercontrollers.todaycyberservices);
