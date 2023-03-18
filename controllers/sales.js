95b519ec70d4fb330b751a2fffe70b8005867bac
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-18 16:29:07 +0300

    finding total in sales

--- controllers/sales.js
+++ controllers/sales.js
@@ -3,83 +3,91 @@ const Stock = require("../models/add-stock");
 
 // Saving new sale
 const newsale = async (req, res) => {
-
   const saledetails = req.body;
 
   const salequantity = saledetails.quantity;
 
   const stocksold = saledetails.productname;
 
-    try {
-        // Finding stock being sold
-        const stock = await Stock.find({productname: stocksold});
+  try {
+    // Finding stock being sold
+    const stock = await Stock.find({ productname: stocksold });
 
-         // Ensure the stock was found
-        if (!stock) {
-          return res.status(400).json({ message: 'Stock not found' });
-        }
-
-        // Checking if stock is enough
-        if(stock[0].quantity<salequantity){
-          return res.status(400).json({ error: 'Not enough stock available' });
-        }
-        
-        // Deduct stock sold
-        stock[0].quantity -= salequantity;
-
-        await stock[0].save();
-
-        // Sales calculations
-        const sellingprice = stock[0].sellingprice;
-        const buyingprice = stock[0]. buyingprice;
-
-        const clientcost = salequantity * sellingprice;
-
-        const buyingcost = salequantity * buyingprice;
-
-        const projection = clientcost - buyingcost;
-
-        const newsale = new Sales({
-          clientsname:saledetails.clientsname,
-          productname:saledetails.productname,
-          quantity:saledetails.quantity,
-          payment:saledetails.payment,
-          transactioncode:saledetails.transactioncode,
-          clienttotal:clientcost,
-          buyingtotal:buyingcost,
-          projection:projection
-        });
-
-        await newsale.save();
-
-        res.json({
-            success: true,
-            message : "New sale registered successfully"
-        })
-        
+    // Ensure the stock was found
+    if (!stock) {
+      return res.status(400).json({ message: "Stock not found" });
     }
-    catch (error) {
 
-        res.json({
-            success: false,
-            message: "There was an error saving the sale",
-            error: error.message
-        })
-    
+    // Checking if stock is enough
+    if (stock[0].quantity < salequantity) {
+      return res.status(400).json({ error: "Not enough stock available" });
     }
-    
-}
+
+    // Deduct stock sold
+    stock[0].quantity -= salequantity;
+
+    await stock[0].save();
+
+    // Sales calculations
+    const sellingprice = stock[0].sellingprice;
+    const buyingprice = stock[0].buyingprice;
+
+    const clientcost = salequantity * sellingprice;
+
+    const buyingcost = salequantity * buyingprice;
+
+    const projection = clientcost - buyingcost;
+
+    const newsale = new Sales({
+      clientsname: saledetails.clientsname,
+      productname: saledetails.productname,
+      quantity: saledetails.quantity,
+      payment: saledetails.payment,
+      transactioncode: saledetails.transactioncode,
+      clienttotal: clientcost,
+      buyingtotal: buyingcost,
+      projection: projection,
+    });
+
+    await newsale.save();
+
+    res.json({
+      success: true,
+      message: "New sale registered successfully",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "There was an error saving the sale",
+      error: error.message,
+    });
+  }
+};
 
 // Fetching all registered sales
 const allsales = (req, res) => {
   Sales.find()
     .sort({ createdAt: 1 })
     .then((registeredsales) => {
+
       let totalsales = registeredsales.length;
+      let totalstocksold =0;
+      let  totalbuyingprice =0;
+      let totalsellingprice =0;
+
+      registeredsales.forEach((sale) =>{
+        totalstocksold +=sale.quantity;
+        totalbuyingprice += sale.buyingtotal;
+        totalsellingprice += sale.clienttotal;;
+
+      })
 
       res.json({
         total: totalsales,
         sales: registeredsales,
+        stocksold:totalstocksold,
+        stocksoldworth:totalbuyingprice,
+        totalearnings:totalsellingprice
       });
     })
 
@@ -103,8 +111,24 @@ const todaysales = (req, res) => {
     .sort({ createdAt: -1 })
     .then((todaysales) => {
       let totalsales = todaysales.length;
+      let totalstocksold =0;
+      let  totalbuyingprice =0;
+      let totalsellingprice =0;
 
-      res.json({ total: totalsales, sales: todaysales });
+      todaysales.forEach((sale) =>{
+        totalstocksold +=sale.quantity;
+        totalbuyingprice += sale.buyingtotal;
+        totalsellingprice += sale.clienttotal;;
+
+      })
+
+      res.json({ 
+        total: totalsales, 
+        sales: todaysales,
+        stocksold:totalstocksold,
+        stocksoldworth:totalbuyingprice,
+        totalearnings:totalsellingprice
+       });
     })
     .catch((error) => {
       console.log(error);
